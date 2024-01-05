3fb863444a75d6aebd7762d45e21055e79aefded
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2024-01-05 04:30:29 -0300

    Add draw wall function

--- include/map.h
+++ include/map.h
@@ -3,6 +3,7 @@
 
     #include "utils.h"
 
-    extern const uint8_t map_collision(const uint8_t *map, int x, int y);
+    extern const uint8_t map_collision( const uint8_t *map, int x, int y );
+    extern void map_draw_walls( SDL_Renderer *renderer, Color_t wallColor );
 
 #endif
--- src/map.cpp
+++ src/map.cpp
@@ -2,8 +2,19 @@
 
 const uint8_t map_collision(const uint8_t *map, int x, int y)
 {
-    if ( x < 0 || y < 0 || x >= MAP_WIDTH || y >= MAP_HEIGHT )
-        return 1;
+    if ( x < 0 || y < 0 || x >= MAP_WIDTH || y >= MAP_HEIGHT ) return 1;
 
     return map[y * MAP_WIDTH + x];
+}
+
+void map_draw_walls( SDL_Renderer *renderer, Color_t wallColor )
+{
+    SDL_SetRenderDrawColor( renderer, whiteColor.r, whiteColor.g, whiteColor.b, whiteColor.a );
+
+    for (int y = 0; y < MAP_HEIGHT; y++)
+        for (int x = 0; x < MAP_HEIGHT; x++)
+        {
+            SDL_Rect wallRect = { x * WALL_SCALE, y * WALL_SCALE, WALL_SCALE, WALL_SCALE };
+            if (map[y * MAP_WIDTH + x]) SDL_RenderFillRect(renderer, &wallRect);
+        }
 }
