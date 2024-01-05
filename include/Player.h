ab47de870f83ea1c145b4616d46613bd14f5041f
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2024-01-05 03:05:38 -0300

    Fix erros and refactor the code of player

--- include/Player.h
+++ include/Player.h
@@ -1,23 +1,34 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
-#include <Raycast.h>
+#include "utils.h"
 
 class Player
 {
     public:
         Player( SDL_FPoint position, float rotate );
 
-        void update( const float dt, const Uint8 *keys, const uint8_t *map );
-        void render( SDL_Renderer* renderer );
+        SDL_FPoint getPosition() const;
 
-        std::vector<float> rays;
+        void update( const float dt, const Uint8 *keys, uint8_t *map );
+        void render( SDL_Renderer *renderer );
+
     private:
         SDL_FPoint position;
 
         float rotate;
         float vel = 6;
         float rotate_vel = 180;
+        std::vector<float> rays;
+
+        bool canMove( const uint8_t *map, float dirX, float dirY, int offset );
+
+        void updatePosition( const float dt, const uint8_t *map );
+        void updateRotation( const float dt, const Uint8 *keys );
+        void updateRays();
+
+        void renderPlayer( SDL_Renderer *renderer );
+        void renderRays( SDL_Renderer *renderer );
 };
 
 #endif
