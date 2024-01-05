79aa7cbdc0aa635f54540584de8d6044d8e33e4c
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2024-01-05 03:44:39 -0300

    Refactoring process

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,48 +1,98 @@
 #include <utils.h>
-#include <player.h>
 
-Player::Player(SDL_FPoint position, float rotate)
+Player::Player(SDL_FPoint position, float rotate) : position( position ), rotate( rotate )
 {
-    this->position = position;
-    this->rotate = rotate;
     rays.resize(RAYS);
 }
 
-void Player::update(const float dt, const Uint8* keys, const uint8_t *map)
+SDL_FPoint Player::getPosition() const { return position; }
+
+int Player::getRayLength() { return rays.size(); }
+int Player::getRay( const int index ) { return rays[index]; }
+
+bool canMove( const uint8_t *map, float dirX, float dirY )
 {
-    rotate += (keys[SDL_SCANCODE_RIGHT] - keys[SDL_SCANCODE_LEFT]) * DEG2RAD(rotate_vel) * dt;
+    auto checkCollision = [&] ( float offsetX, float offsetY )
+    {
+        return map_collision( map, dirX, dirY );
+    };
 
-    SDL_FPoint spd = {0, 0};
-    spd.x += (keys[SDL_SCANCODE_UP] - keys[SDL_SCANCODE_DOWN]) * cos(rotate) * vel * dt;
-    spd.y += (keys[SDL_SCANCODE_UP] - keys[SDL_SCANCODE_DOWN]) * sin(rotate) * vel * dt;
-    
-    if (map_collision(map, position.x + spd.x - 1, position.y - 1) == 0 && map_collision(map, position.x + spd.x + 1, position.y - 1) == 0 &&
-        map_collision(map, position.x + spd.x + 1, position.y + 1) == 0 && map_collision(map, position.x + spd.x - 1, position.y + 1) == 0)
-        position.x += spd.x;
-    if (map_collision(map, position.x - 1, position.y + spd.y - 1) == 0 && map_collision(map, position.x + 1, position.y + spd.y - 1) == 0 &&
-        map_collision(map, position.x + 1, position.y + spd.y + 1) == 0 && map_collision(map, position.x - 1, position.y + spd.y + 1) == 0)
-        position.y += spd.y;
+    return (
+        checkCollision( -1, -1 ) && checkCollision( 1, -1 ) &&
+        checkCollision( -1, 1 ) && checkCollision( 1, 1 )
+    );
+}
 
+void Player::updateRays( const uint8_t *map )
+{
     for (int i = 0; i <= RAYS; i++)
     {
         int id = i - RAYS / 2;
         float r = rotate + DEG2RAD(id / 4);
-        rays[i] = Raycast(position, r, map);
+        rays[i] = Raycast( position, r, map );
     }
 }
 
-void Player::render(SDL_Renderer* renderer)
+void Player::updatePosition( const float dt, const uint8_t *map, const Uint8 *keys )
 {
-    SDL_FRect r = {position.x * SCALE - SCALE / 2, position.y * SCALE - SCALE/2, SCALE, SCALE};
-    SDL_RenderFillRectF(renderer, &r);
-    
+    int verticalInput = keys[SDL_SCANCODE_UP] - keys[SDL_SCANCODE_DOWN];
+    int horizontalInput = keys[SDL_SCANCODE_UP] - keys[SDL_SCANCODE_DOWN];
+
+    SDL_FPoint speed = {
+        (verticalInput) * cos(rotate) * vel * dt,
+        (horizontalInput) * sin(rotate) * vel * dt
+    };
+
+    if ( canMove( map, speed.x, -1 ) && canMove( map, speed.x, 1 ) ) position.x += speed.x;
+    if ( canMove( map, speed.y, -1 ) && canMove( map, speed.y, 1 ) ) position.y += speed.y;
+}
+
+void Player::updateRotation( const float dt, const Uint8 *keys )
+{
+    rotate += (keys[SDL_SCANCODE_RIGHT] - keys[SDL_SCANCODE_LEFT]) * DEG2RAD(rotate_vel) * dt;
+}
+
+void Player::update( const float dt, const Uint8* keys, const uint8_t *map )
+{
+
+    updateRotation( dt, keys );
+    updatePosition( dt, map, keys );
+    updateRays( map );
+}
+
+void Player::renderPlayer( SDL_Renderer *renderer )
+{
+    SDL_FRect playerRect = {
+        position.x * SCALE - SCALE / 2,
+        position.y * SCALE - SCALE / 2,
+        SCALE, SCALE
+    };
+
+    SDL_RenderFillRectF(renderer, &playerRect);
+}
+
+void Player::renderRays( SDL_Renderer *renderer )
+{
+    renderPlayer( renderer );
+
     for (int i = 0; i <= rays.size(); i++)
     {
         int id = i - RAYS / 2;
         float r = rotate + DEG2RAD(id / 4);
         float dist = rays[i];
 
-        SDL_RenderDrawLineF(renderer, position.x * SCALE, position.y * SCALE,
-                                        (position.x + cos(r) * dist) * SCALE, (position.y + sin(r) * dist) * SCALE);
+        SDL_RenderDrawLineF(
+            renderer,
+            position.x * SCALE,
+            position.y * SCALE,
+            (position.x + cos(r) * dist) * SCALE,
+            (position.y + sin(r) * dist) * SCALE
+        );
     }
+}
+
+void Player::render( SDL_Renderer *renderer )
+{
+    renderPlayer( renderer );
+    renderRays( renderer );
 }
