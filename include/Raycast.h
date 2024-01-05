d88f40aee692be292056add21cfd2c76fb4a8b61
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2024-01-05 05:05:21 -0300

    Remove MAX_STEPS from raycast header file

--- include/Raycast.h
+++ include/Raycast.h
@@ -3,8 +3,6 @@
 
 #include "utils.h"
 
-#define MAX_STEPS 20
-
 extern float Raycast( SDL_FPoint position, float rotate, const uint8_t *map );
 extern void raycast_draw_rays( Player *player, SDL_Renderer *renderer );
 
--- include/utils.h
+++ include/utils.h
@@ -24,6 +24,7 @@
 
     #define WALL_SCALE 2
     #define MAX_RAYS 320
+    #define MAX_STEPS 20
 
     #define PI_DIV_180 0.01745329251994329576923690
 
