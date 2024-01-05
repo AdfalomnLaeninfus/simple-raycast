e1f5c095d6d1172295ca18fec3d236477737fb04
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2024-01-04 21:38:19 -0300

    Add map reference for function

--- src/map.cpp
+++ src/map.cpp
@@ -1,6 +1,6 @@
 #include <map.h>
 
-const uint8_t map_collision(int x, int y)
+const uint8_t map_collision(const uint8_t *map, int x, int y)
 {
     if ( x < 0 || y < 0 || x >= MAP_WIDTH || y >= MAP_HEIGHT )
         return 1;
