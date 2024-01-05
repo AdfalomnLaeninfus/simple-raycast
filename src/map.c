034fb23103665b8410f2ef59056795e241465755
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2024-01-04 21:23:54 -0300

    Rename file for cpp project

--- src/map.c
+++
@@ -1,9 +0,0 @@
-#include <map.h>
-
-const uint8_t map_collision(int x, int y)
-{
-    if ( x < 0 || y < 0 || x >= MAP_WIDTH || y >= MAP_HEIGHT )
-        return 1;
-
-    return map[y * MAP_WIDTH + x];
-}
